42c67ec5df776bf8e08b8bf359b7cbd9f964def5
sohaibgv <sohaib.ezzahir@icloud.com>
2020-08-21 15:06:24 +0200

    Ejected app

--- android/app/src/main/java/com/examplensfwjs/nl/generated/BasePackageList.java
+++ android/app/src/main/java/com/examplensfwjs/nl/generated/BasePackageList.java
@@ -7,11 +7,14 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
+        new expo.modules.camera.CameraPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.gl.GLPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.location.LocationPackage(),
